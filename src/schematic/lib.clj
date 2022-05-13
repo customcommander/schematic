e6f5b7e776b256d8245e1b694b7853a5550bea1e
customcommander <hello@spinjs.com>
2022-05-13 14:44:14 +0100

    update defgen docstring

--- src/schematic/lib.clj
+++ src/schematic/lib.clj
@@ -1,9 +1,8 @@
 (ns schematic.lib)
 
 (defmacro defgen
-  "Blockly generator. Takes a block and returns a string.
-  Must return normal Clojure data. It will be converted to
-  JavaScript and serialised automatically."
+  "Little sugar for code generator functions.
+  Automatically transform Clojure data to JSON documents."
   [f] `(fn [block#]
          (-> (~f block#)
              (cljs.core/clj->js)
